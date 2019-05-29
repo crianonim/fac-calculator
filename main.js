1c01a2542f1746528f5eacf32e6aa38bbbc45d52
Jan Skowronski <crianonim@gmail.com>
2019-05-29 11:59:56 +0100

    Buttons handler added and analyseInput

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 window.addEventListener("load",init);
 
 function init(){
-    
+    document.querySelectorAll('main button').forEach(el=>el.addEventListener("click",handleButton))
+}
+
+function handleButton(event){
+    let input=event.target.innerText;
+    let inputObj=analyseInput(input);
+    console.log(inputObj)
+}
+
+function analyseInput(input){
+    let inputObj={input};
+
+    return inputObj;
 }
