ce3d4a72b85036be5964733da5158282b57549d6
Jan Skowronski <crianonim@gmail.com>
2019-05-29 13:31:39 +0100

    renamy analyse to process and process function handles basic operations on integers

--- main.js
+++ main.js
@@ -1,17 +1,75 @@
-window.addEventListener("load",init);
+const OPERATORS = ["/", "+", "-", "x"];
+const DIGITS = "1234567890".split('');
+const OPERATIONS = {
+    "/": (a, b) => a / b,
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
+    "x": (a, b) => a * b
+}
+let total = 0;
+let operation = null;
+let currentInput = "0";
+let afterEquals = false; // after '=' so 
+let DISPLAY;
 
-function init(){
-    document.querySelectorAll('main button').forEach(el=>el.addEventListener("click",handleButton))
+window.addEventListener("load", init);
+
+function init() {
+    DISPLAY = document.getElementById("display");
+    document.querySelectorAll('main button').forEach(el => el.addEventListener("click", handleButton))
+    updateDisplay();
 }
 
-function handleButton(event){
-    let input=event.target.innerText;
-    let inputObj=analyseInput(input);
-    console.log(inputObj)
+function handleButton(event) {
+    // console.log("BEFORE", total, operation, currentInput)
+    let input = event.target.innerText;
+    processInput(input);
 }
 
-function analyseInput(input){
-    let inputObj={input};
+function processInput(input) {
+    if (DIGITS.includes(input)) {
+        if (currentInput == "0" || afterEquals) {
+            currentInput = input;
+            afterEquals = false;
+        } else {
+            currentInput += input;
+        }
+        console.log(currentInput);
+        updateDisplay();
 
-    return inputObj;
+    } else if (OPERATORS.includes(input)) {
+        if (operation) {
+            evaluateOperation();
+        } else {
+            total = Number(currentInput)
+        }
+        operation = OPERATIONS[input];
+        updateDisplay(total);
+        currentInput = "0";
+
+
+    } else if (input == "Clear") {
+        total = 0;
+        operation = null;
+        currentInput = "0";
+        updateDisplay();
+    } else if (input == "=") {
+        if (operation) {
+            evaluateOperation();
+            updateDisplay(total);
+            currentInput = total + "";
+            afterEquals = true;
+        } else {
+
+        }
+
+    }
+}
+function evaluateOperation() {
+    console.log(total, Number(currentInput))
+    total = operation(total, Number(currentInput));
+    operation = null;
+}
+function updateDisplay(value = currentInput) {
+    DISPLAY.innerText = value + "";
 }
